b0f14a09bd71041fb982d252797514ad5c0f1ffb
gili-julio <giliardoplay@gmail.com>
2024-10-17 19:03:33 -0300

    Spring Initializr

---
+++ src/main/java/com/web2/acem/AcemApplication.java
@@ -0,0 +1,13 @@
+package com.web2.acem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web2/acem/AcemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web2.acem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
