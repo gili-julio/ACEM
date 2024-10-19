457857f3726f0cc0ed1f4d282af1f8f3b714708c
gili-julio <giliardoplay@gmail.com>
2024-10-19 12:15:25 -0300

    tirando dependencia não utilizada

--- src/main/java/com/web2/acem/controller/HomeController.java
+++ src/main/java/com/web2/acem/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.web2.acem.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
